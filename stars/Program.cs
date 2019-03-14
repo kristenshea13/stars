556443793b2c1633b1644b4fbab42f9ce5bdf281
Kristen Shea <kstar61@live.com>
2019-03-14 18:39:50 -0500

    /

--- stars/Program.cs
+++ stars/Program.cs
@@ -2,11 +2,61 @@
 
 namespace stars
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //********
+            //*******
+            //******
+            //*****
+            //****
+            //***
+            //**
+            //*
+
+            for (int i = 0; i <= 7; i++)
+            {
+                string stars = new string('*', 8 - i);
+                string spaces = new string(' ', i);
+
+                Console.WriteLine($"{stars}{spaces}");
+            }
+
+            Console.ReadLine();
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //******
+            //*******
+
+            for (int i = 1; i <= 7; i++)
+            {
+                string stars = new string('*', i);
+                Console.WriteLine(stars);
+            }
+
+            Console.ReadLine();
+
+            //       *
+            //      **
+            //     ***
+            //    ****
+            //   *****
+            //  ******
+
+            for (int i = 1; i <= 6; i++)
+            {
+                string spaces = new string(' ', 6 - i);
+
+                string stars = new string('*', i);
+
+                Console.WriteLine($"{spaces}{stars}");
+            }
+            Console.ReadLine();
         }
     }
 }
